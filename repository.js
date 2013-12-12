af01a59eeec49194956cdb9e574f483311f02c0d
tikkystore <tikky_return@hotmail.com>
2013-12-12 22:26:37 +0700

    Create repository.js

---
+++ repository.js
@@ -0,0 +1,34 @@
+var node = {
+    nodes: [
+        /* AI(Artificial Intelligence) */
+        {
+            name: "AI(Artificial Intelligence)",
+            link: "http://na5cent.blogspot.com/search/label/AI%28Artificial%20Intelligence%29",
+            nodes: [
+                {
+                    name: "Back Propagation Algorithm",
+                    link: "http://na5cent.blogspot.com/2011/03/back-propagation-algorithm.html"
+                }
+            ]
+        },
+        
+        /* AI(Artificial Intelligence) */
+        {
+            name: "AI(Artificial Intelligence)",
+            link: "http://na5cent.blogspot.com/search/label/AI%28Artificial%20Intelligence%29",
+            nodes: [
+                {
+                    name: "Back Propagation Algorithm",
+                    link: "http://na5cent.blogspot.com/2011/03/back-propagation-algorithm.html"
+                }
+            ]
+        }
+        
+        
+        
+       
+       
+  /*Credit : Nascent.blogspot.com */     
+       
+    ]
+};
